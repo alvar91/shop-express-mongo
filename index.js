8ad5cdf92b21bcc0a91a158bec23dca74f95d2f5
Aleksey <aleksey91scorp@bk.ru>
2020-05-31 14:14:34 +0300

    user added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const homeRoutes = require("./routes/home");
 const cardRoutes = require("./routes/card");
 const addRoutes = require("./routes/add");
 const coursesRoutes = require("./routes/courses");
+const User = require("./models/user");
 
 const app = express();
 
@@ -17,6 +18,16 @@ const hbs = exphbs.create({
 app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "views");
+
+app.use(async (req, res, next) => {
+  try {
+    const user = await User.findById("5ed38e060f1a1232d8ae01da");
+    req.user = user;
+    next();
+  } catch (e) {
+    console.log(e);
+  }
+});
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
@@ -32,9 +43,18 @@ async function start() {
   try {
     const url = `mongodb+srv://alvar91:u8MhVqQ.!*GNumg@cluster0-lhrpi.mongodb.net/shop`;
     await mongoose.connect(url, {
-      useUnifiedTopology: true,
       useNewUrlParser: true,
+      useFindAndModify: false,
     });
+    const candidate = await User.findOne();
+    if (!candidate) {
+      const user = new User({
+        email: "aleksey91scorp@bk.ru",
+        name: "Aleksey",
+        cart: { items: [] },
+      });
+      await user.save();
+    }
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
